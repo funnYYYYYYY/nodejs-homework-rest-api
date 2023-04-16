54c410a705807fdf149e946ceea65c8a550507d4
Pavlo <pasam9300@gmail.com>
2023-04-16 18:48:56 +0200

    add subscription, add favorite-filtr, add logout, add paginations

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,4 +1,13 @@
 const register = require("./register");
 const login = require("./login");
+const getCurrent = require("./getCurrent");
+const logOut = require("./logOut");
+const updateSubscription = require("./updateSubscription");
 
-module.exports = { register, login };
+module.exports = {
+  register,
+  login,
+  getCurrent,
+  logOut,
+  updateSubscription,
+};
--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -2,7 +2,19 @@ const { Contact } = require("../../models");
 
 const listContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const contacts = await Contact.find({ owner }, "-createdAt -updatedAt");
+  const { page = 1, limit = 10, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const filter = { owner };
+
+  if (favorite) {
+    filter.favorite = favorite === "true";
+  }
+
+  const contacts = await Contact.find(filter, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
+
   res.json(contacts);
 };
 
