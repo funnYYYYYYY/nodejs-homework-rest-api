6e7d11ad0cdcb613d85e48ab70c093dfccf18489
Pavlo <pasam9300@gmail.com>
2023-04-23 13:21:03 +0200

    added gimp to give avatar dimensions 250x250added gimp to give avatar size 250x250

--- middlewares/updateAvatar.js
+++ middlewares/updateAvatar.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { User } = require("../models");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
@@ -8,6 +9,10 @@ const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, filename } = req.file;
   const avatarName = `${_id}_${filename}`;
+
+  const image = await Jimp.read(tempUpload);
+  await image.resize(250, 250);
+  await image.writeAsync(tempUpload);
 
   const resultUpdate = path.join(avatarsDir, avatarName);
 
